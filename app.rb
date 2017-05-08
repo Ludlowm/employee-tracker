79f2db9f6e9ede3deec0c89b1e0328cd89b11e44
Marko Ludlow and Sean Pierce <marko@>
2017-05-08 15:02:05 -0700

    add CRUD functionality for Division class

--- app.rb
+++ app.rb
@@ -22,10 +22,42 @@ end
 # route to add a division to db
 post("/add_division") do
   division = params['add-division']
-  Division.create({:title => division})
+  Division.create(title: division)
   redirect('/divisions')
+  @divisions = Division.all
+  erb(:divisions)
 end
 
+# route to see employee list
 get("/employees") do
+@employees = Employee.all
   erb(:employees)
+end
+
+# route to add an employee to db
+post("/add_employee") do
+  employee = params['add-employee']
+  Employee.create(name: employee)
+  redirect('/employees')
+end
+
+# path to individual division
+get("/division/:id") do
+  @division = Division.find(params['id'].to_i)
+  erb(:division)
+end
+
+# path tp update division name
+patch("/edit_division/:id") do
+  new_title = params['edit-title']
+  division = Division.find(params['id'].to_i)
+  division.update(title: new_title)
+  redirect("/division/#{division.id}")
+end
+
+# path to delete a division
+delete("/delete_division/:id") do
+  division = Division.find(params['id'].to_i)
+  division.delete
+  redirect("/divisions")
 end
